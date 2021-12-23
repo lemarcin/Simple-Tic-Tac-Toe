75c6c17175a1814d29e88f54beeca32fae91f82c
Marcin Lewicki <lemarcin@gmail.com>
2021-12-23 12:40:27 -0500

    Work on project. Stage 2/5: The user is the gamemaster

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "XOXOXOXXO";
+        System.out.print("Enter cells:");
+        String str = new java.util.Scanner(System.in).nextLine();
         final int n = 3;
         char[][] grid = new char[n][n];
         for (int i = 0, k = 0; i < n; i++) {
@@ -15,11 +16,14 @@ public class Main {
     }
     public static void print(char grid[][]) {
         int n = 3;
+        System.out.println("---------");
         for (int i = 0; i < n; i++) {
+            System.out.print("| ");
             for (int j = 0; j < n; j++) {
                 System.out.print(grid[i][j] + " ");
             }
-            System.out.println("");
+            System.out.println("|");
         }
+        System.out.println("---------");
     }
 }
