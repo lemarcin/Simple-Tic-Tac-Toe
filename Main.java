3f0e0df2ba0f574724e0aa94ac62ca27bc441b2d
Marcin Lewicki <lemarcin@gmail.com>
2021-12-27 09:56:02 -0500

    Work on project. Stage 4/5: First move!

--- Main.java
+++ Main.java
@@ -1,30 +1,15 @@
 package tictactoe;
 
-public class Main {
-    public static void main(String[] args) {
-        char[][] grid = enterCells();
-        print(grid);
-        System.out.println(stateOfGame(grid));
-    }
+import java.util.Scanner;
 
-    public static String stateOfGame(char[][] grid) {
-        String stateOfGame = "State of Game";
-        int noOX = noOX(grid);
-        int noO = noOX % 10;
-        int noX = noOX / 10;
-        String whoWins = whoWins(grid);
-        if (Math.abs(noX - noO) > 1 || whoWins.equals("true true")) {
-            stateOfGame = "Impossible";
-        } else if (noO + noX == 9 && whoWins.equals("false false")) {
-            stateOfGame = "Draw";
-        } else if (noO + noX < 9 && whoWins.equals("false false")) {
-            stateOfGame = "Game not finished";
-        } else if (whoWins.equals("true false")) {
-            stateOfGame = "O wins";
-        } else if (whoWins.equals("false true")) {
-            stateOfGame = "X wins";
-        }
-        return stateOfGame;
+public class Main {
+
+    public static void main(String[] args) {
+        char[][] grid = enterCells().clone();
+        print(grid);
+        char nextMove = 'X';
+        enterCoordinates(grid, nextMove);
+        print(grid);
     }
 
     public static char[][] enterCells() {
@@ -66,33 +51,50 @@ public class Main {
         return o + 10 * x;
     }
 
-    public static String whoWins(char[][] grid) {
-        boolean xWins = false;
-        boolean oWins = false;
-        int xh = 0, oh = 0, xv = 0, ov = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                xh += grid[i][j] == 'X' ? 1 : 0;
-                oh += grid[i][j] == 'O' ? 1 : 0;
-                xv += grid[j][i] == 'X' ? 1 : 0;
-                ov += grid[j][i] == 'O' ? 1 : 0;
+    public static void enterCoordinates(char[][] grid, char nextMove) {
+        do {
+            System.out.print("Enter the coordinates: ");
+            String str = new Scanner(System.in).nextLine();
+            int x = 0;
+            int y = 0;
+            if (str.matches("[123] [123]")) {
+                x = Integer.parseInt(str.split(" ")[0]) - 1;
+                y = Integer.parseInt(str.split(" ")[1]) - 1;
+                if (grid[x][y] == 'X' || grid[x][y] == 'O') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                } else {
+                    grid[x][y] = nextMove;
+                    break;
+                }
+            } else if (str.matches("\\d+ \\d+")) {
+                System.out.println("Coordinates should be from 1 to 3!");
+            } else {
+                System.out.println("You should enter numbers!");
             }
-            xWins = xh == 3 || xWins;
-            oWins = oh == 3 || oWins;
-            xWins = xv == 3 || xWins;
-            oWins = ov == 3 || oWins;
-            xh = 0; oh = 0; xv = 0; ov = 0;
+        } while (true);
+    }
+
+    public static String stateOfGame(char[][] grid) {
+        boolean xWin = false;
+        boolean oWin = false;
+        int xs = 0;
+        int os = 0;
+        for (int i = 0; i < grid.length; i++) {
+            int row = 0;
+            int col = 0;
+            int lDiag = 0;
+            int rDiag = 0;
+            for (int j = 0; j < grid[i].length; j++) {
+                xs += grid[i][j] == 'X' ? 1 : 0;
+                os += grid[i][j] == 'O' ? 1 : 0;
+                row += grid[i][j];
+                col += grid[j][i];
+                lDiag += grid[j][j];
+                rDiag += grid[j][2 - j];
+            }
+            xWin = xWin || row == 264 || col == 264 || lDiag == 264 || rDiag == 264;
+            oWin = oWin || row == 237 || col == 237 || lDiag == 237 || rDiag == 237;
         }
-        for (int i = 0, j = 0, k = grid.length - 1; i < grid.length ; i++, j++, k--) {
-            xh += grid[i][j] == 'X' ? 1 : 0;
-            oh += grid[i][j] == 'O' ? 1 : 0;
-            xv += grid[i][k] == 'X' ? 1 : 0;
-            ov += grid[i][k] == 'O' ? 1 : 0;
-        }
-        xWins = xh == 3 || xWins;
-        oWins = oh == 3 || oWins;
-        xWins = xv == 3 || xWins;
-        oWins = ov == 3 || oWins;
-        return oWins + " " + xWins;
+        return xWin ? "X wins" : oWin ? "O wins" : xs + os == 9 ? "Draw": "Game not finished";
     }
 }
